151f604da26f93a735ab001e4d1c64bfda292b61
Nina Tsertsvadze <Tsernina@yandex.ru>
2026-03-05 20:38:25 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,7 @@
+3.	@Test
+4.	public void checkIsAdultWhenAgeIsMoreThan18True() {
+5.	    Program program = new Program();
+6.	    boolean isAdult = program.checkIsAdult(19);
+7.	    assertEquals("что-то пошло не так", true, isAdult);
+8.	    // Напиши код здесь
+9.	}
